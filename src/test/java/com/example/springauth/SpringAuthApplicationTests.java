0c6e364c40144c4bb1480ba07d1a985a531292a5
heeezni <hjin3456@naver.com>
2025-08-29 17:14:17 +0900

    🎉 Start : 프로젝트 시작

---
+++ src/test/java/com/example/springauth/SpringAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("dev")
+@SpringBootTest
+class SpringAuthApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
